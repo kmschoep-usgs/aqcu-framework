564ce25f423c98e272f94366710b8b0962d0d94b
Steinich, Dave R. <drsteini@usgs.gov>
2018-03-30 08:58:43 -0500

    Add nanoseconds to begining instants

--- src/main/java/gov/usgs/aqcu/parameter/RequestParameters.java
+++ src/main/java/gov/usgs/aqcu/parameter/RequestParameters.java
@@ -95,7 +95,7 @@ public class RequestParameters {
 	}
 
 	protected Pair<Instant,Instant> waterYearToReportPeriod(Integer waterYear) {
-		Instant reportStartTime = Instant.from(LocalDateTime.of(waterYear-1,10,1,0,0,0).toInstant(ZoneOffset.UTC));
+		Instant reportStartTime = Instant.from(LocalDateTime.of(waterYear-1,10,1,0,0,0,0).toInstant(ZoneOffset.UTC));
 		Instant reportEndTime = Instant.from(LocalDateTime.of(waterYear,9,30,23,59,59,999999999).toInstant(ZoneOffset.UTC));
 
 		return new ImmutablePair<Instant,Instant>(reportStartTime, reportEndTime);
@@ -103,7 +103,7 @@ public class RequestParameters {
 
 	protected Pair<Instant,Instant> lastMonthsToReportPeriod(Integer lastMonths) {
 		LocalDate nowDate = LocalDate.now().minusMonths(lastMonths);
-		Instant reportStartTime = LocalDateTime.of(nowDate.getYear(), nowDate.getMonth(), 1, 0, 0, 0).toInstant(ZoneOffset.UTC);
+		Instant reportStartTime = LocalDateTime.of(nowDate.getYear(), nowDate.getMonth(),1,0,0,0,0).toInstant(ZoneOffset.UTC);
 		Instant reportEndTime = LocalDate.now().atTime(23,59,59,999999999).toInstant(ZoneOffset.UTC);
 
 		return new ImmutablePair<Instant,Instant>(reportStartTime, reportEndTime);
